7b1b91eb47be8adf99e0b975f74122026d7f88a0
Joon-S <niceweather94@gmail.com>
2019-02-16 23:33:59 +0900

    element : compare with Franc and Dollar

--- src/main/java/chapter1/Money.java
+++ src/main/java/chapter1/Money.java
@@ -2,4 +2,11 @@ package chapter1;
 
 public class Money {
     protected int amount;
+
+    @Override
+    public boolean equals(Object obj) {
+        Money money = (Money) obj;
+        return amount == money.amount
+                && getClass().equals(money.getClass());
+    }
 }
