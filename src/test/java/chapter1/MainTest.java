876ac8d6ed8bab8a5a39d387d2046994c205f821
Joon-S <niceweather94@gmail.com>
2019-02-16 18:15:01 +0900

    element : Dollar equals()

--- src/test/java/chapter1/MainTest.java
+++ src/test/java/chapter1/MainTest.java
@@ -2,7 +2,7 @@ package chapter1;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class MainTest {
 
@@ -13,5 +13,11 @@ public class MainTest {
         assertEquals(20, product.amount);
         five.times(3);
         assertEquals(20, product.amount);
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
