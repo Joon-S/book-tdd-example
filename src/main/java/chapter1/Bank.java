ccccc4b7487b9251a465fd2be4f24e7879fa58b3
Joon-S <niceweather94@gmail.com>
2019-02-23 10:39:15 +0900

    completed chapter 14

--- src/main/java/chapter1/Bank.java
+++ src/main/java/chapter1/Bank.java
@@ -1,7 +1,23 @@
 package chapter1;
 
+import java.util.Hashtable;
+
 public class Bank {
+
+    private Hashtable rates = new Hashtable();
+
     public Money reduce(Expression source, String to) {
-        return source.reduce(to);
+        Bank bank = new Bank();
+        return source.reduce(bank, to);
+    }
+
+    public void addRate(String from, String to, int rate) {
+        rates.put(new Pair(from, to), new Integer(rate));
+    }
+
+    public int rate(String from, String to) {
+        if (from.equals(to)) return 1;
+        Integer rate = (Integer) rates.get(new Pair(from, to));
+        return rate.intValue();
     }
 }
---
+++ src/main/java/chapter1/Pair.java
@@ -0,0 +1,22 @@
+package chapter1;
+
+public class Pair {
+    private String from;
+    private String to;
+
+    public Pair(String from, String to) {
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    public int hashCode() {
+        return 0; //terrible hash code
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        Pair pair = (Pair) obj;
+        return from.equals(pair.from) && to.equals(pair.to);
+    }
+}
